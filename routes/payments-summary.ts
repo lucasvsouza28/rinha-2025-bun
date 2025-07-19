84fb157e510add2370e943fc71c34f13d6230fba
Lucas Souza <lucasvsouza28@gmail.com>
2025-07-19 15:28:11 -0300

    feat: carrega resumo de pagamentos via redis

--- routes/payments-summary.ts
+++ routes/payments-summary.ts
@@ -1,15 +1,38 @@
 import { redis } from "bun";
 import { Elysia } from "elysia";
 
+type PaymentSummary = {
+    totalRequests: number;
+    totalAmount: number;
+};
+
 const paymentsSummary = new Elysia()
     .get('/payments-summary', async () => {
-        const payments = await redis.keys('payments:*');
-        const summary = await Promise.all(payments.map(async (key) => {
+        const summary: {
+            default: PaymentSummary;
+            fallback: PaymentSummary;
+        } = {
+            default: {
+                totalAmount: 0,
+                totalRequests: 0
+            },
+            fallback: {
+                totalAmount: 0,
+                totalRequests: 0
+            }
+        };
+        const keys = await redis.keys('payments:*:*');
+        await Promise.all(keys.map(async (key) => {
             const paymentData = await redis.get(key);
-            return JSON.parse(paymentData || '{}');
+            const payment = JSON.parse(paymentData || '{}');
+            const isDefault = key.startsWith('payments:default:');
+            const store = isDefault ? summary['default'] : summary['fallback'];
+            
+            store.totalRequests += 1;
+            store.totalAmount += payment.amount || 0;
         }));
 
-        return summary
+        return summary;
     });
 
 export default paymentsSummary;
