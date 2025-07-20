510cb2f64a0a5f9c66eda00ff663b4c09eaddce1
Lucas Souza <lucasvsouza28@gmail.com>
2025-07-20 02:19:01 -0300

    fix(purge-payments): limpa fila de pagamentos

--- routes/purge-payments.ts
+++ routes/purge-payments.ts
@@ -1,12 +1,15 @@
 import { redis } from 'bun';
 import { Elysia } from 'elysia';
+import PaymentsQueue from "@queues/payment-processing";
 
 const purgePayments = new Elysia()
   .get('/purge-payments', async () => {
     const keys = await redis.keys(`payments:*`);
     Promise.all(keys.map(k => {
-      redisedis.del(k)
+      redis.del(k)
     }));
+    
+    PaymentsQueue.empty();
   });
 
 export default purgePayments;
