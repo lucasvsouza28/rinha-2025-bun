829c5f4e10f62b6cf68b82d4c3e0a4b414883e75
Lucas Souza <lucasvsouza28@gamil.com>
2025-07-22 10:39:18 -0300

    fix(payments-summary) from/to optional filters

--- routes/payments-summary.ts
+++ routes/payments-summary.ts
@@ -6,41 +6,65 @@ type PaymentSummary = {
   totalAmount: number;
 };
 
-const paymentsSummary = new Elysia()
-  .get('/payments-summary', async ({ query: { from, to } }) => {
+const paymentsSummary = new Elysia().get(
+  "/payments-summary",
+  async ({ query: { from, to } }) => {
+    const fromDate = from ? Date.parse(from) : undefined;
+    const toDate = to ? Date.parse(to) : undefined;
     const summary: {
       default: PaymentSummary;
       fallback: PaymentSummary;
     } = {
       default: {
         totalAmount: 0,
-        totalRequests: 0
+        totalRequests: 0,
       },
       fallback: {
         totalAmount: 0,
-        totalRequests: 0
-      }
+        totalRequests: 0,
+      },
     };
-    const keys = await redis.keys('payments:*:*');
-    await Promise.all(keys.map(async (key) => {
-      const paymentData = await redis.get(key);
-      const payment = JSON.parse(paymentData || '{}') as { amount: number; requestedAt: Date; };
-      const isDefault = key.startsWith('payments:default:');
-      const store = isDefault ? summary['default'] : summary['fallback'];
+    const keys = await redis.keys("payments:*:*");
+    await Promise.all(
+      keys.map(async (key) => {
+        const paymentData = await redis.get(key);
+        const payment = JSON.parse(paymentData || "{}") as {
+          correlationId: string;
+          amount: number;
+          requestedAt: Date;
+        };
+        const isDefault = key.startsWith("payments:default:");
+        const store = isDefault ? summary["default"] : summary["fallback"];
+        console.log("comparing dates", {
+          from,
+          fromDate,
+          to,
+          toDate,
+          id: payment.correlationId,
+          requestedAt: payment.requestedAt,
+        });
 
-      if ((!from || payment.requestedAt >= from) || (!to || payment.requestedAt <= to)) {
-        store.totalRequests += 1;
-        store.totalAmount += payment.amount || 0;
-      }
-    }));
+        if (
+          !fromDate ||
+          payment.requestedAt >= fromDate ||
+          !toDate ||
+          payment.requestedAt <= toDate
+        ) {
+          console.log('date in range')
+          store.totalRequests += 1;
+          store.totalAmount += payment.amount || 0;
+        }
+      }),
+    );
 
     return summary;
-  }, {
+  },
+  {
     query: t.Object({
-      from: t.Optional(t.Date()),
-      to: t.Optional(t.Date())
-    })
-  }
-  );
+      from: t.Optional(t.String()),
+      to: t.Optional(t.String()),
+    }),
+  },
+);
 
 export default paymentsSummary;
