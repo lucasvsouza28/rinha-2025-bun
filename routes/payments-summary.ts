e721e81d81f370d7439f02423fb48d79b6a7c09a
Lucas Souza <lucasvsouza28@gamil.com>
2025-07-24 12:04:24 -0300

    chore: lint

--- routes/payments-summary.ts
+++ routes/payments-summary.ts
@@ -1,11 +1,11 @@
 import { Elysia, t } from "elysia";
-import { PaymentRedisRepository, PaymentSqlRepository } from "@repositories/payments";
+import { PaymentSqlRepository } from "@repositories/payments";
 
 const paymentsSummary = new Elysia().get(
   "/payments-summary",
   async ({ query: { from, to } }) => {
-    //const repo = new PaymentRedisRepository();
     const repo = new PaymentSqlRepository();
+
     return repo.getPaymentsSummary(from, to);
   },
   {
